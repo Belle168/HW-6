2d87532ce2ca843e5053fa42aced01c08607e05d
Burul22 <burula.termechikova@gmail.com>
2022-01-10 19:18:19 +0300

    I create my project

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import Expenses from './components/Expenses';
+
+function App() {
+    const expenses = [
+        {
+            id: 'el',
+            title: 'Total Paper',
+            amount: 1.5,
+            date: new Date(2020, 7, 14),
+        },
+        {
+            id: 'el2',
+            title: 'Car Insurance',
+            amount: 200,
+            date: new Date(2021, 2, 28),
+     },
+     {
+        id: 'el3',
+        title: 'Gift',
+        amount: 1300,
+        date: new Date(2021, 8, 16),
+     },
+     {
+        id: 'el4',
+        title: 'Income Tax',
+        amount: 790,
+        date: new Date(2021, 11, 20),
+     },
+    ];
+    
+
+    return (
+        <div className='App'>
+            <h1>hello</h1>
+<Expenses expenses = {expenses}/>
+    </div>
+    );
+}
+
+export default App;
