ce0d74012deb387d2e8eae29d7b57a7a3e45a293
Burul22 <burula.termechikova@gmail.com>
2022-01-18 13:41:09 +0300

    For Halida sister

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import './App.css';
-import Expenses from './components/Expenses';
+import Expenses from './components/expense-tracker/Expenses';
 
 function App() {
     const expenses = [
         {
-            id: 'el',
+            id: 'e1',
             title: 'Total Paper',
             amount: 1.5,
             date: new Date(2020, 7, 14),
         },
         {
-            id: 'el2',
+            id: 'e2',
             title: 'Car Insurance',
             amount: 200,
             date: new Date(2021, 2, 28),
      },
      {
-        id: 'el3',
+        id: 'e3',
         title: 'Gift',
         amount: 1300,
         date: new Date(2021, 8, 16),
      },
      {
-        id: 'el4',
+        id: 'e4',
         title: 'Income Tax',
         amount: 790,
         date: new Date(2021, 11, 20),
---
+++ src/components/new-expenses/NewExpenses.js
@@ -0,0 +1,9 @@
+import './NewExpenses.css'
+const NewExpenses = () => {
+
+return <div className='new-expense'>
+
+</div>
+};
+
+export default NewExpenses;
